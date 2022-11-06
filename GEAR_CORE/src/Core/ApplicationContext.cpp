196c25c85ab5ce6f315de436c429c09755eed2e0
Andrew Richards <andyjrichards93@gmail.com>
2022-11-06 13:50:00 +0000

    Updated Application Name to include "GPU Validation" when enabled.

--- GEAR_CORE/src/Core/ApplicationContext.cpp
+++ GEAR_CORE/src/Core/ApplicationContext.cpp
@@ -48,6 +48,7 @@ ApplicationContext::ApplicationContext(CreateInfo* pCreateInfo)
 	m_CI.applicationName += ": x64";
 	m_CI.applicationName += debugBuild ? "/Debug" : "";
 	m_CI.applicationName += GraphicsAPI::IsD3D12() ? " - D3D12" : " - Vulkan";
+	m_CI.applicationName += m_CI.commandLineOptions.debugValidationLayers ? " - GPU Validation" : "";
 
 	Context::CreateInfo contextCI;
 	contextCI.applicationName = m_CI.applicationName;
